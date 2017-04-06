c07029b13acc7553d49ce908effbe406f36301ad
unknown <you@example.com>
2017-04-06 22:13:14 +0300

    fonts

--- app/include/header.php
+++ app/include/header.php
@@ -1,1 +1,76 @@
-header.php
+<header>
+	<div class="az-menu-cart">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="clearfix">
+						<div class="pull-left">
+							<nav>
+								<a href="#" class="az-co">Меню</a>
+								<ul class="az-menu">
+									<li>
+										<a href="#">Каталог товаров <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+										<ul>
+											<li>
+												<a href="#">Плодовые <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul>
+													<li>
+														<a href="#">Отзывы</a>
+													</li>
+													<li>
+														<a href="#">Статьи</a>
+													</li>
+													<li>
+														<a href="#">Доставка и оплата</a>
+													</li>
+													<li>
+														<a href="#">Контакты</a>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<a href="#">Ягодные <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul>
+													<li>
+														<a href="#">Отзывы</a>
+													</li>
+													<li>
+														<a href="#">Статьи</a>
+													</li>
+													<li>
+														<a href="#">Доставка и оплата</a>
+													</li>
+													<li>
+														<a href="#">Контакты</a>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<a href="#">Декаративные <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											</li>
+											<li>
+												<a href="#">Розы <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Отзывы</a>
+									</li>
+									<li>
+										<a href="#">Статьи</a>
+									</li>
+									<li>
+										<a href="#">Доставка и оплата</a>
+									</li>
+									<li>
+										<a href="#">Контакты</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
--- app/js/common2.js
+++ app/js/common2.js
@@ -1,3 +1,39 @@
 $(function() {
 
+	$('.az-co').click(function(){
+		$(this).toggleClass('az-close').next('.az-menu').slideToggle(300);
+	});
+
+	$('.az-menu a').click(function(){
+		if(window.matchMedia( "(max-width: 768px)" ).matches){
+			if($(this).next('ul').length>0&&$(this).next('ul').css('display')=='none'){
+				$(this).parent().siblings("li").each(function(i){
+					if($(this).children('ul').length>0){
+						$(this).children('ul').slideUp(300);
+					}
+				});
+				$(this).next('ul').slideDown(300);
+				return false;
+			}else if($(this).next('ul').length>0&&$(this).next('ul').css('display')=='block'){
+				$(this).next('ul').slideUp(300);
+				return false;
+			}
+		} //else if (window.matchMedia( "(min-width: 768px)" ).matches && window.matchMedia( "(max-width: 1200px)" ).matches){
+      //     if($(this).hasClass('active')){
+      //         $(this).removeClass('active');
+      //         return false;
+      //     } else {
+      //         $('.az-menu>li>a').removeClass('active');
+      //         $(this).addClass('active');
+      //         if($(this).next('ul').length>0){
+      //           return false;
+      //         }
+      //     }
+      // } else{
+      //   if($(this).next('ul').length>0){
+      //     return false;
+      //   }
+      // }
+      // return false;
+	});
 });
