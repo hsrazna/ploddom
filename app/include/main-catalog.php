ab78e30d27e0dae816585d4b8a2c43ade3c9e8f3
hsrazna <anzar.terek@gmail.com>
2017-04-07 16:46:19 +0300

    catalog

--- app/include/main-catalog.php
+++ app/include/main-catalog.php
@@ -1,1 +1,17 @@
-main-catalog.php
+<main class="az-catalog">
+	<div class="az-catalog-wrap">
+		<div class="az-catalog-col">
+			<a href="#">
+				<div class="az-atalog-item">
+					<div class="az-ci-img">
+						<div class="az-ci-img-inner">
+							<img src="img/cat-img2.jpg">
+						</div>
+					</div>
+					<div class="az-ci-title"><span>другие плодовые</span><span>деревья</span></div>
+					<div class="az-ci-link"><span></span></div>
+				</div>
+			</a>
+		</div>
+	</div>
+</main>
