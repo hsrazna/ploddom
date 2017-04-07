ed24a6fe7ebfa945be38d4ffe50a26a646b976af
iwebnal <iwebnal@gmail.com>
2017-04-07 19:54:38 +0300

    a

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,14 +1,118 @@
-<section class="ah-footer-all">
+<section class="ah-footer-all hidden-xs">
     <div class="container">
     	<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 				<div class="ah-footer">
-					<div class="ah-footlogo">
-						<a href="/"><img src="img/logo_pd.svg" alt="footerlogo"></a>
+					<div class="ah-footer1">
+						<div class="ah-footlogo">
+							<a href="/"><img src="img/logo_pd.svg" alt="footerlogo"></a>
+						</div>
+						<div class="ah-payment">
+							<span class="ah-payment-span">Оплата</span>
+							<ul class="ah-payment-list">
+								<li>
+									<a href="#">
+										<img src="img/ah-visa.png" alt="visa">
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="img/ah-master.png" alt="master">
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="img/ah-sber.png" alt="sber">
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="img/ah-yandex.png" alt="yandex">
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
-					<div class="ah-payment">
-						<span class="ah-payment-span">Оплата</span>
-						<ul class="ah-payment-list">
+					<div class="ah-footer2 hidden-sm hidden-xs">
+						<div class="ah-soc">
+							<span class="ah-soc-span">Мы в соц. сетях:</span>
+							<ul class="ah-soc-list">
+								<li>
+									<a href="#" class="ah-ok">
+										<!-- <img src="img/ok.svg" alt="ok"> -->
+									</a>
+								</li>
+								<li>
+									<a href="#" class="ah-insta">
+										<!-- <img src="img/insta.svg" alt="insta"> -->
+									</a>
+								</li>
+								<li>
+									<a href="#" class="ah-fb">
+										<!-- <img src="img/fb.svg" alt="fb"> -->
+									</a>
+								</li>
+								<li>
+									<a href="#" class="ah-vk">
+										<!-- <img src="img/vk.svg" alt="vk"> -->
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="ah-company">
+							<a href="#" class="ah-company-link"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="ah-footer-bottom hidden-xs hidden-sm">
+	<span class="ah-footbottom-span">Copyright © 2016 «ПлодовыйДом.РФ»  Саженцы плодовых растений. Все права защищены.</span>
+</section>
+<section class="ah-footer-all1 hidden-lg hidden-md hidden-xs">
+    <div class="container">
+    	<div class="row">
+			<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer2">
+					<div class="ah-soc">
+						<span class="ah-soc-span">Мы в соц. сетях:</span>
+						<ul class="ah-soc-list">
+							<li>
+								<a href="#" class="ah-ok"></a>
+							</li>
+							<li>
+								<a href="#" class="ah-insta"></a>
+							</li>
+							<li>
+								<a href="#" class="ah-fb"></a>
+							</li>
+							<li>
+								<a href="#" class="ah-vk"></a>
+							</li>
+						</ul>
+					</div>
+					<div class="ah-company">
+						<a href="#" class="ah-company-link"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="ah-footer-bottom hidden-lg hidden-md hidden-xs">
+	<span class="ah-footbottom-span">Copyright © 2016 «ПлодовыйДом.РФ»  Саженцы плодовых растений. Все права защищены.</span>
+</section>
+<section class="ah-footer-all2 hidden-lg hidden-md hidden-sm">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer11">
+					<div class="ah-payment ah-payment320">
+						<span class="ah-payment-span ah-payment-span320">Оплата</span>
+						<ul class="ah-payment-list ah-payment-list320">
 							<li>
 								<a href="#">
 									<img src="img/ah-visa.png" alt="visa">
@@ -31,41 +135,34 @@
 							</li>
 						</ul>
 					</div>
-					<div class="ah-soc">
-						<span class="ah-soc-span">Мы в соц. сетях:</span>
+					<div class="ah-soc ah-soc320">
+						<span class="ah-soc-span ah-soc-span320">Мы в социальных сетях:</span>
 						<ul class="ah-soc-list">
 							<li>
-								<a href="#" class="ah-ok">
-									<!-- <img src="img/ok.svg" alt="ok"> -->
-								</a>
+								<a href="#" class="ah-ok"></a>
 							</li>
 							<li>
-								<a href="#" class="ah-insta">
-									<!-- <img src="img/insta.svg" alt="insta"> -->
-								</a>
+								<a href="#" class="ah-insta"></a>
 							</li>
 							<li>
-								<a href="#" class="ah-fb">
-									<!-- <img src="img/fb.svg" alt="fb"> -->
-								</a>
+								<a href="#" class="ah-fb"></a>
 							</li>
 							<li>
-								<a href="#" class="ah-vk">
-									<!-- <img src="img/vk.svg" alt="vk"> -->
-								</a>
+								<a href="#" class="ah-vk"></a>
 							</li>
 						</ul>
 					</div>
-					<div class="ah-company">
-						<a href="#" class="ah-company-link"></a>
+					<div class="ah-company ah-company320">
+						<a href="#" class="ah-company-link ah-company-link320"></a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-
-
+<section class="ah-footer-bottom hidden-lg hidden-md hidden-sm">
+	<span class="ah-footbottom-span">Copyright © 2016 «ПлодовыйДом.РФ»  Саженцы плодовых растений. Все права защищены.</span>
+</section>
 
 
 
