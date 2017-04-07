d6a7ec916c23920b89fa5d6b86c10ff30235b0be
iwebnal <iwebnal@gmail.com>
2017-04-07 10:55:57 +0300

    a

--- app/include/slider.php
+++ app/include/slider.php
@@ -1,30 +1,46 @@
-<div class="ah-slider">
-	<div class="item">
-		<div class="ah-itemwrapp">
-			<span class="ah-slider-title1">Лучшие саженцы</span><br>
-			<span class="ah-slider-title2">плодовых деревьев</span>
-			<span class="ah-slider-subtitle">для вашего сада</span>
-			<a href="#" class="ah-slider-link">подробнее</a>
-			<div class="ah-slider-imgbox">
-				<img src="img/ah-slider-men.png" alt="men">
+<div class="ah-sliderwrapp">
+	<div class="ah-slider">
+		<div class="item">
+			<div class="ah-subitem">
+				<div class="ah-itemwrapp">
+					<span class="ah-slider-title1">Лучшие саженцы</span><br>
+					<span class="ah-slider-title2">плодовых деревьев</span>
+					<span class="ah-slider-subtitle">для вашего сада</span>
+					<a href="#" class="ah-slider-link">подробнее</a>
+					<div class="ah-slider-imgbox">
+						<img src="img/ah-slider-men.png" alt="men">
+					</div>
+				</div>
 			</div>
+			<div class="clear"></div>
 		</div>
-		<div class="clear"></div>
-	</div>
-	<div class="item">
-		<div class="ah-itemwrapp">
-			<span class="ah-slider-title">Лучшие саженцы<br>плодовых деревьев</span>
-			<span class="ah-slider-subtitle">для вашего сада</span>
-			<a href="#" class="ah-slider-link">подробнее</a>
+		<div class="item">
+			<div class="ah-subitem">
+				<div class="ah-itemwrapp">
+					<span class="ah-slider-title1">Лучшие саженцы</span><br>
+					<span class="ah-slider-title2">плодовых деревьев</span>
+					<span class="ah-slider-subtitle">для вашего сада</span>
+					<a href="#" class="ah-slider-link">подробнее</a>
+					<div class="ah-slider-imgbox">
+						<img src="img/ah-slider-men.png" alt="men">
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
 		</div>
-		<div class="clear"></div>
-	</div>
-	<div class="item">
-		<div class="ah-itemwrapp">
-			<span class="ah-slider-title">Лучшие саженцы<br>плодовых деревьев</span>
-			<span class="ah-slider-subtitle">для вашего сада</span>
-			<a href="#" class="ah-slider-link">подробнее</a>
+		<div class="item">
+			<div class="ah-subitem">
+				<div class="ah-itemwrapp">
+					<span class="ah-slider-title1">Лучшие саженцы</span><br>
+					<span class="ah-slider-title2">плодовых деревьев</span>
+					<span class="ah-slider-subtitle">для вашего сада</span>
+					<a href="#" class="ah-slider-link">подробнее</a>
+					<div class="ah-slider-imgbox">
+						<img src="img/ah-slider-men.png" alt="men">
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
 		</div>
-		<div class="clear"></div>
 	</div>
 </div>
