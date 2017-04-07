cfb47f2fd4721047146cfb0558dbffd63deff35c
iwebnal <iwebnal@gmail.com>
2017-04-07 18:26:48 +0300

    a

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,5 +1,84 @@
-<div class="ah-footer">
-	<div class="ah-footlogo">
-		<img src="img/logo_pd.svg" alt="footerlogo">
+<section class="ah-footer-all">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer">
+					<div class="ah-footlogo">
+						<a href="/"><img src="img/logo_pd.svg" alt="footerlogo"></a>
+					</div>
+					<div class="ah-payment">
+						<span class="ah-payment-span">Оплата</span>
+						<ul class="ah-payment-list">
+							<li>
+								<a href="#">
+									<img src="img/ah-visa.png" alt="visa">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/ah-master.png" alt="master">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/ah-sber.png" alt="sber">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/ah-yandex.png" alt="yandex">
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="ah-soc">
+						<span class="ah-soc-span">Мы в соц. сетях:</span>
+						<ul class="ah-soc-list">
+							<li>
+								<a href="#" class="ah-ok">
+									<!-- <img src="img/ok.svg" alt="ok"> -->
+								</a>
+							</li>
+							<li>
+								<a href="#" class="ah-insta">
+									<!-- <img src="img/insta.svg" alt="insta"> -->
+								</a>
+							</li>
+							<li>
+								<a href="#" class="ah-fb">
+									<!-- <img src="img/fb.svg" alt="fb"> -->
+								</a>
+							</li>
+							<li>
+								<a href="#" class="ah-vk">
+									<!-- <img src="img/vk.svg" alt="vk"> -->
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="ah-company">
+						<a href="#" class="ah-company-link"></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
