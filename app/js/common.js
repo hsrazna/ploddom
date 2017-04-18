374ce3799a68f6675269eefcd63d3028074dbfb8
iwebnal <iwebnal@gmail.com>
2017-04-18 13:50:25 +0300

    a

--- app/js/common.js
+++ app/js/common.js
@@ -1,42 +1,50 @@
 $(function() {
 
-	$('.ah-slider').owlCarousel({
-	    loop: true,
-	    margin: 0,
-	    items: 1,
-	    nav: true,
-	    navText:['<span class="ah-arrow-r"></span>','<span class="ah-arrow-l"></span>']
-	});
+	if($('.ah-slider').length>0){
+		$('.ah-slider').owlCarousel({
+		    loop: true,
+		    margin: 0,
+		    items: 1,
+		    nav: true,
+		    navText:['<span class="ah-arrow-r"></span>','<span class="ah-arrow-l"></span>']
+		});
+	}
 
-	$('.ah-menu-list li').click(function(){
-		$(this).find('.ah-submenu-list').slideToggle(500);
+	if($('.ah-menu-list li').length>0){
+		$('.ah-menu-list li').click(function(){
+			$(this).find('.ah-submenu-list').slideToggle(500);
 
-		return false;
-	});
+			return false;
+		});
+	}
 
-	$('.ah-catbutton-list li').click(function(){
-		$(this).find('.ah-menu-list320').slideToggle(500);
+	if($('.ah-catbutton-list li').length>0){
+		$('.ah-catbutton-list li').click(function(){
+			$(this).find('.ah-menu-list320').slideToggle(500);
 
-		return false;
-	});
+			return false;
+		});
+	}
 
+	if($('.ah-cardslider').length>0){
+		$('.ah-cardslider').owlCarousel({
+	        items:1,
+	        loop:false,
+	        center:true,
+	        margin:0,
+	        nav: true,
+	        navText:['<span class="arrow-left1"></span>','<span class="arrow-right1"></span>'],
+	        URLhashListener:true,
+	        autoplayHoverPause:true,
+	        startPosition: 'URLHash'
+	    });
+	}
 
-	$('.ah-cardslider').owlCarousel({
-        items:1,
-        loop:false,
-        center:true,
-        margin:0,
-        nav: true,
-        navText:['<span class="arrow-left1"></span>','<span class="arrow-right1"></span>'],
-        URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'URLHash'
-    });
-
-	$('.selectpicker').selectpicker({
-	  size: 4
-	});
-
+	if($('.selectpicker').length>0){
+		$('.selectpicker').selectpicker({
+		  size: 4
+		});
+	}
 
 
 });
