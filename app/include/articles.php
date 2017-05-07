afbf2430d76b0053eb154e7743c3aca06413b6c8
iwebnal <iwebnal@gmail.com>
2017-05-07 14:34:51 +0300

    a

---
+++ app/include/articles.php
@@ -0,0 +1,130 @@
+<div class="ah-cardwrapper">
+	<div class="az-breadcrumb hidden-xs">
+		<ul>
+			<li><a href="#">Главная</a></li>
+			<li><a href="#">Каталог</a></li>
+			<li><a href="#">Плодовые</a></li>
+			<li><a>Черешня</a></li>
+		</ul>
+	</div>
+	<span class="az-title-wrap ah-title-wrap"><span class="az-title">Статьи</span></span>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img1.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img1.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img1.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img1.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-articles-item">
+		<div class="ah-articles-imgbox">
+			<img src="img/ah-articles-img1.jpg" class="img-responsive" alt="articl">
+		</div>
+		<div class="ah-articles-textbox">
+			<span class="ah-articles-itemtitle">В Тюмени родители смогут отслеживать перемещение  детей по GPS</span>
+			<p class="ah-articles-text hidden-xs">Авторы проекта считают, что большая осведомленность родителей сможет значительно повысить детскую безопасность. Основой новой технологии является мобильное приложение, которые отслеживает местоположение ребенка благодаря специальным часам со встроенной сим-картой.</p>
+			<a href="#" class="ah-articles-link">Подробнее</a>
+		</div>
+	</div>
+	<div class="ah-pagination-box">
+		<a href="#" class="ah-pagination-link_arrl"><</a>
+		<a href="#" class="ah-pagination-link_prev">Назад</a>
+		<ul class="ah-pagination-list">
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li class="ah-pagnumber"><a href="#">5</a></li>
+			<li class="ah-pagnumber"><a href="#">6</a></li>
+			<li class="ah-pagnumber"><a href="#">7</a></li>
+			<li class="ah-pagnumber"><a href="#">8</a></li>
+			<li class="ah-pagnumber"><a href="#">9</a></li>
+			<li><a href="#">...</a></li>
+		</ul>
+		<a href="#" class="ah-pagination-link_next">Дальше</a>
+		<a href="#" class="ah-pagination-link_arrr">></a>
+	</div>
+</div>
+
