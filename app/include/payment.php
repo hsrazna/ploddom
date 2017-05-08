21cac7bf4b38c1293a212f822a29dd39f9f396de
iwebnal <iwebnal@gmail.com>
2017-05-08 09:59:50 +0300

    s

---
+++ app/include/payment.php
@@ -0,0 +1,139 @@
+<div class="ah-cardwrapper">
+	<div class="az-breadcrumb hidden-xs">
+		<ul>
+			<li><a href="#">Главная</a></li>
+			<li><a href="#">Каталог</a></li>
+			<li><a href="#">Плодовые</a></li>
+			<li><a>Черешня</a></li>
+		</ul>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<span class="az-title-wrap ah-title-wrap"><span class="az-title">ДОСТАВКА и оплата</span></span>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<span class="ah-payment-subtitle hidden-lg hidden-md">Доставка по России</span>
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<span class="ah-payment-subtitle hidden-sm hidden-xs">Доставка по России</span>
+							<p class="ah-payment-text">Мы с удовольствием доставим Ваши покупки и подарки во многие города России. Минимальная сумма заказа по России - 1200 рублей. Действует служба курьерской доставки и доставка в пункты самовывоза!</p>
+							<p class="ah-payment-text">Для Москвы и некоторых городов предлагается опция "доставка в день заказа" с возможностью получить заказ в течении нескольких часов. Индивидуальные букеты доставляются в премиальной букетной коробке!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<span class="ah-payment-subtitle hidden-lg hidden-md">Доставка по Москве</span>
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img1.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<span class="ah-payment-subtitle hidden-sm hidden-xs">Доставка по Москве</span>
+							<p class="ah-payment-text">Осуществляется на следующий рабочий день, следующий за днем оформления заказа. Стоимость доставки до двери - 199 рублей. Минимальная сумма заказа - 1200 рублей. Доставка осуществляется в будние дни c 9:00 до 22:00. Опция "доставка в день заказа" позволяет Вам заказать курьера в выходные и праздничные дни с доставкой в течении нескольких часов! Пожалуйста, согласуйте возможность срочной доставки подарка при оформлении заказа. Доставка до пункта выдачи  - 99 рублей!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<span class="ah-payment-subtitle hidden-lg hidden-md">Доставка по Санкт-Петербургу</span>
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img2.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<span class="ah-payment-subtitle hidden-xs hidden-sm">Доставка по Санкт-Петербургу</span>
+							<p class="ah-payment-text">Осуществляется в течении двух рабочих дней со дня оформления заказа. Стоимость доставки до двери - 199 рублей. Минимальная сумма заказа - 1200 рублей. Доставка осуществляется в будние дни c 9:00 до 22:00. Опция экспресс доставки позволяет Вам заказать курьера в выходные и праздничные дни. Пожалуйста, согласуйте эту возможность при оформлении заказа. Доставка в Бизнес-центры или торговые центры осуществляется до входа или ресепшн. Доставка до пункта выдачи - 99 рублей!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<span class="ah-payment-subtitle hidden-lg hidden-md">Доставка по Великому Новгороду</span>
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img3.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<span class="ah-payment-subtitle hidden-xs hidden-sm">Доставка по Великому Новгороду</span>
+							<p class="ah-payment-text">Осуществляется на следующий рабочий день. Стоимость доставки до двери - 150 рублей. Доставка осуществляется в удобный для Вас трехчасовой интервал. Доставка бесплатная при оформлении доставки в пункт выдачи (любой магазин сети).</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<span class="az-title-wrap ah-title-wrap"><span class="az-title">ЗАКАЗ</span></span>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img4.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<p class="ah-payment-text">Мы обрабатываем заказы с понедельника по пятницу с 10:00 до 19:00. Заказы, поступающие в выходные, обрабатываются в понедельник. После оформления заказа на сайте с Вами свяжется сотрудник, чтобы уточнить детали заказа и доставки.</p>
+							<p class="ah-payment-text">Вы так же можете использовать сервис "Подарки от Заварки" для дистанционного поздравления своих родных и близких в разных городах России. Мы доставим выбранный Вами подарок прямо в руки и в заранее согласованное время.</p>
+							<p class="ah-payment-text">Вы можете задать любой интересующий Вас вопрос по чаю, кофе и другим товарам чайной лавки ЗАВАРКА на нашей страничке вконтакте, направить запрос по электронной почте или позвонить по номеру 8 800-301-7678 (звонок бесплатный).</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<span class="az-title-wrap ah-title-wrap"><span class="az-title">ОПЛАТА</span></span>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="ah-payment-wrapp">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
+						<div class="ah-payment-imgbox">
+							<img src="img/ah-payment-img5.png" class="img-responsive" alt="payment">
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+						<div class="ah-payment-textbox">
+							<p class="ah-payment-text">Вы можете оплатить доставку наличными курьеру при получении заказа или воспользоваться онлайн оплатой картами платежных систем VISA и MasterCard. Работа с юридическими лицами. Выставим все необходимые документы (счет, товарная накладная ТОРГ-12). Пожалуйста, обсудите эту возможность при подтверждении заказа.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
